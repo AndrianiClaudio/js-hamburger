cb45adbaed87c45a3418aac3aeb138f3d8c86c71
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-11-22 15:53:50 +0100

    rimosso errore di indentazione

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ const iClose = document.querySelector('i.fas.fa-times');
 // accquisisco menu da mostrare e nascondere
 const hamburgerMenu = document.querySelector('div.hamburger-menu') ;
 // Al click (fa-bars) mostra menu
-                iOpen.addEventListener('click',function(){
-                    hamburgerMenu.classList.add('active');
-                });
+iOpen.addEventListener('click',function(){
+hamburgerMenu.classList.add('active');
+});
 // Al click (fa-times) nascondi menu
 iClose.addEventListener('click',function(){
     // Rimuovi classe active a (div.hamburger-menu)
