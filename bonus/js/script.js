90f6568eeb124ac0c4b04b486b91761ed69f787f
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-11-22 16:56:31 +0100

    Bonus: colore sfondo header

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,1 +1,6 @@
-console.log('Link js corretto');
+// modifica sconfo barra in rosso
+const btnBgBar = document.querySelector('#button-container .btn-bg-bar-color');
+const header = document.querySelector('header');
+btnBgBar.addEventListener('click',function() {
+    header.classList.add('bg-green');
+});
