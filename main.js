2c33222f8c6494e6634af3d692d08b906925dbd8
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-11-22 16:20:58 +0100

    rimozione punto su queryselector(i).cartella bonus

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 // Acquisisco i (icona apri menu)
 const iOpen = document.querySelector('.header-right i.fas.fa-bars');
 // acquisisco i (icona chiudi menu)
-const iClose = document.querySelector('.hamburger-menu .i.fas.fa-times');
+const iClose = document.querySelector('.hamburger-menu i.fas.fa-times');
 // accquisisco menu da mostrare e nascondere
-const hamburgerMenu = document.querySelector('div.hamburger-menu') ;
+const hamburgerMenu = document.querySelector('div.hamburger-menu');
 // Al click (fa-bars) mostra menu
-iOpen.addEventListener('click',function(){
-hamburgerMenu.classList.add('active');
+iOpen.addEventListener('click', function () {
+    hamburgerMenu.classList.add('active');
 });
 // Al click (fa-times) nascondi menu
-iClose.addEventListener('click',function(){
+iClose.addEventListener('click', function () {
     // Rimuovi classe active a (div.hamburger-menu)
     hamburgerMenu.classList.remove('active');
 });
