6ba6b62b833a6cc87f1efe4ed1a7725b6a09ff0c
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-11-22 17:40:54 +0100

    bonus: new item nasconsto (per ora infinite volte)

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,6 +1,19 @@
 // modifica sconfo barra in rosso
-const btnBgBar = document.querySelector('#button-container .btn-bg-bar-color');
+const btnBgBar = document.querySelector('#button-container .btn-bg-color');
 const header = document.querySelector('header');
 btnBgBar.addEventListener('click',function() {
     header.classList.add('bg-green');
+});
+
+//aggiungi li al menu cliccando su un bottone
+const btnPrivate = document.getElementById('btn-private-menu');
+// const headerUl = document.querySelector('.header-right ul');
+// const hamburgerUl = document.querySelector('.hamburger-menu ul');
+const headerLastChild = document.querySelector('.header-right ul > li:last-child');
+const hamburgerLastChild = document.querySelector('.hamburger-menu ul > li:last-child');
+// console.log(headerLastChild,hamburgerLastChild);
+console.log(headerLastChild.innerHTML);
+btnPrivate.addEventListener('click',function(){
+    headerLastChild.innerHTML += "<li><a hfre='#'>Secret Item</a></li>";
+    hamburgerLastChild.innerHTML += "<li><a hfre='#' style='margin-top: 50px;'>Secret Item</a></li>";
 });
