86834455f7de690eed5fa3637fa7dc0dbcefd527
AndrianiClaudio <claudioandriani1994@gmail.como>
2021-11-22 15:26:34 +0100

    creazione elementi interessati. impostazione click

---
+++ main.js
@@ -0,0 +1,10 @@
+//  OBIETTIVO: MOSTRARE NASCORE IL MENU PRINCIPALE
+// acquisisco il primo i del nostro dom: quello equivalente all'icona apri/chiudi menu
+const i = document.querySelector('i');
+console.log(i);
+//al click su i andrá visualizzato il menu nascoto di default dal css .hamburger-menu
+const hamburgerMenu = document.querySelector('div.hamburger-menu') ;
+console.log(hamburgerMenu);
+i.addEventListener('click',function(){
+    console.log('click effettuato');
+});
